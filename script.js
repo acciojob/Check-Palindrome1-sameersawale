ff72461470daca4eb7bee6a93f556b9b47d89a72
sameersawale <60250589+sameersawale@users.noreply.github.com>
2023-05-14 14:09:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,18 @@
 
 function palindrome(str){
 
+	int n=str.length;
+	let i=0;
+	let j=n-1;
+	let isPalindrome=true;
+	while(i<=j){
+		if(str[i]!==str[j]){
+			isPalindrome=false;
+			break;
+		}
+		i++;
+		j--;
+	}
+	console.log(isPalindrome ? "true" : "false");
 }
 module.exports = palindrome
